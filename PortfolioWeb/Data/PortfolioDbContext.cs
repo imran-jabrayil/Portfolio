7f2c235181d77495582c491089efbf99071c01a5
Imran Jabrayilov <c.imran2001@yandex.com>
2024-03-03 20:02:33 +0400

    Authentication added

--- PortfolioWeb/Data/PortfolioDbContext.cs
+++ PortfolioWeb/Data/PortfolioDbContext.cs
@@ -1,5 +1,6 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace PortfolioWeb.Data;
 
-public sealed class PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : DbContext(options);
+public sealed class PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : IdentityDbContext(options);
