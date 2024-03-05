94b74c750d42cc5eaa7a45c63ef4679ba7fdd2cc
Imran Jabrayilov <c.imran2001@yandex.com>
2024-03-05 11:05:50 +0400

    Redundant using removed

--- PortfolioWeb/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ PortfolioWeb/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -5,7 +5,6 @@
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
