47fb11c18b87e4cbbae3a93e3e46be5bda5ec8dd
Imran Jabrayilov <c.imran2001@yandex.com>
2025-03-04 19:45:49 +0400

    added WorkExperience

---
+++ Portfolio/Models/WorkExperience.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Models;
+
+public class WorkExperience
+{
+    public required int Id { get; set; }
+    public required string Company { get; set; }
+    public required string CompanyUrl { get; set; }
+    public required string Position { get; set; }
+    public required string Country { get; set; }
+    public required string City { get; set; }
+    public required DateTime StartDate { get; set; }
+    public DateTime? TerminationDate { get; set; }
+}
